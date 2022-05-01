8f38c64c690762edfcf6112b4b7ab411599a8237
FlowerF <1647231392@qq.com>
2022-05-01 16:40:11 +0800

    feat: 添加登录页面的样式

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,8 @@ import App from "./App.vue";
 import router from "./router";
 
 import ElementPlus from "element-plus";
+import "normalize.css";
+import "./styles/index.scss";
 
 const app = createApp(App);
 
--- src/views/LoginView.vue
+++ src/views/LoginView.vue
@@ -7,7 +7,7 @@ import { Avatar } from "@element-plus/icons-vue";
   <div class="login-container">
     <el-form class="login-form">
       <div class="title-container">
-        <h3>用户登录</h3>
+        <h3 class="title">用户登录</h3>
       </div>
       <!-- 用户名 -->
       <el-form-item>
@@ -43,4 +43,84 @@ import { Avatar } from "@element-plus/icons-vue";
   </div>
 </template>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+$bg: #2d3a4b;
+$dark_gray: #888;
+$light_gray: #eee;
+$cursor: #fff;
+
+.login-container {
+  min-height: 100vh;
+  width: 100%;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  background-color: $bg;
+  overflow: hidden;
+  .login-form {
+    max-width: 500px;
+    min-width: 400px;
+    overflow: hidden;
+    :deep(.el-form-item) {
+      border: 1px solid rgba(255, 255, 255, 0.1);
+      background-color: rgba(0, 0, 0, 0.1);
+      border-radius: 5px;
+      color: #454545;
+    }
+    :deep(.el-input) {
+      display: inline-block;
+      height: 47px;
+      width: 85%;
+      .el-input__wrapper {
+        background: transparent;
+        border: none;
+        outline: none;
+        box-shadow: none;
+        width: 100%;
+        height: 100%;
+        input {
+          width: 100%;
+          background: transparent;
+          border: none;
+          border-radius: 0px;
+          padding: 12px 5px 12px 0px;
+          color: $light_gray;
+          caret-color: $cursor;
+        }
+      }
+    }
+  }
+
+  .svg-container {
+    padding: 6px 0px 6px 15px;
+    color: $dark_gray;
+    vertical-align: middle;
+    display: inline-block;
+  }
+
+  .title-container {
+    position: relative;
+
+    .title {
+      font-size: 26px;
+      color: $light_gray;
+      margin: 0px auto 40px auto;
+      text-align: center;
+      font-weight: bold;
+    }
+  }
+
+  .show-password {
+    position: absolute;
+    right: 10px;
+    top: 7px;
+    font-size: 16px;
+    color: $dark_gray;
+    cursor: pointer;
+    user-select: none;
+  }
+  .login-button {
+    width: 100%;
+  }
+}
+</style>
