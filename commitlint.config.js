ce20ddb9348a2d8a2b56df1fd5b0edc2248d47f9
FlowerF <1647231392@qq.com>
2022-05-01 14:53:16 +0800

    chore: 添加commitlint和husky

---
+++ commitlint.config.js
@@ -0,0 +1,23 @@
+// eslint-disable-next-line no-undef
+module.exports = {
+  extends: ["@commitlint/config-conventional"],
+  rules: {
+    "type-enum": [
+      2,
+      "always",
+      [
+        "feat",
+        "fix",
+        "docs",
+        "style",
+        "refactor",
+        "perf",
+        "test",
+        "chore",
+        "revert",
+        "build",
+      ],
+    ],
+    "subject-case": [0],
+  },
+};
